8b4e5ddcc005a416636112927f4b76f16756fbcc
quanphan2906 <hoangquanphan2906@gmail.com>
2020-03-07 17:12:37 +0700

    Init app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Route } from "react-router-dom" 
+
+function App() {
+  return (
+    <BrowserRouter>  
+      <div className="App">
+        <div></div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
